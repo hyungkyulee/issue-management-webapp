8417ebb6fb5386b8a965f34bc117dc4655c6c3ff
hyungkyulee <hyungkyu.lee@outlook.com>
2022-04-20 13:30:33 +0100

    Add project files.

---
+++ IssueManagement.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace IssueManagement.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
