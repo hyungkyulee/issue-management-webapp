cfc09f8856320cba3f58408b41359e91b3e68b7d
hyungkyulee <hyungkyu.lee@outlook.com>
2022-04-30 00:10:13 +0100

    feat: add ef migrations and database updated

---
+++ IssueManagement.Web/Data/Migrations/20220429230124_InheritedBaseEntity.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace IssueManagement.Web.Data.Migrations
+{
+    public partial class InheritedBaseEntity : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
